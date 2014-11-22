1481588b15e2446e7798951bc57492801af8664d
Alin Eugen Deac <aedart@gmail.com>
2014-11-22 15:35:24 +0100

    Exception to be thrown, if an undefined property is being attempted to be accessed

---
+++ src/Exception/UndefinedPropertyException.php
@@ -0,0 +1,15 @@
+<?php namespace Aedart\Overload\Exception;
+
+use LogicException;
+
+/**
+ * Undefined Property Exception
+ *
+ * Throw this exception if a non-existent property is
+ * attempted to be accessed / obtained
+ * 
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ */
+class UndefinedPropertyException extends LogicException{
+    
+}
