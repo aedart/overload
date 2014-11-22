6b51e3f084deafaeacf3bd77d178d0d185dc1a38
Alin Eugen Deac <aedart@gmail.com>
2014-11-22 16:07:08 +0100

    Started work on property overload trait - should be composed of several other traits

---
+++ src/Traits/PropertyOverloadTrait.php
@@ -0,0 +1,14 @@
+<?php namespace Aedart\Overload\Traits;
+
+use Aedart\Overload\Traits\GetterInvokerTrait;
+use Aedart\Overload\Traits\SetterInvokerTrait;
+
+/**
+ * @todo Description of PropertyOverloadTrait
+ * 
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ */
+trait PropertyOverloadTrait {
+    
+    use GetterInvokerTrait, SetterInvokerTrait;
+}
