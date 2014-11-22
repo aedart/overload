af67b3172adbc01d1858c57ee70df95a1c8d358f
Alin Eugen Deac <aedart@gmail.com>
2014-11-22 16:06:15 +0100

    Started work on setter

---
+++ src/Traits/SetterInvokerTrait.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Aedart\Overload\Traits;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of SetterInvokerTrait
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ */
+trait SetterInvokerTrait {
+    //put your code here
+}
