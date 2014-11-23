a6b49cd4e7ffa1b1ac885c8838750d6655b44f77
Alin Eugen Deac <aedart@gmail.com>
2014-11-23 15:22:13 +0100

    Trait now uses isset- and unset-invoker traits - Still missing a description for this trait

--- src/Traits/PropertyOverloadTrait.php
+++ src/Traits/PropertyOverloadTrait.php
@@ -2,13 +2,20 @@
 
 use Aedart\Overload\Traits\GetterInvokerTrait;
 use Aedart\Overload\Traits\SetterInvokerTrait;
+use Aedart\Overload\Traits\IssetInvokerTrait;
+use Aedart\Overload\Traits\UnsetInvokerTrait;
 
 /**
+ * Property Overload Trait
+ * 
  * @todo Description of PropertyOverloadTrait
+ * 
+ * @see \Aedart\Overload\Interfaces\PropertyOverloadable
+ * @see \Aedart\Overload\Traits\Helper\PropertyAccessibilityTrait
  * 
  * @author Alin Eugen Deac <aedart@gmail.com>
  */
 trait PropertyOverloadTrait {
     
-    use GetterInvokerTrait, SetterInvokerTrait;
+    use GetterInvokerTrait, SetterInvokerTrait, IssetInvokerTrait, UnsetInvokerTrait;
 }
