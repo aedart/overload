97b0d827353cbc94f6f0754a9230b72d903b7d8e
Alin Eugen Deac <aedart@gmail.com>
2014-11-23 15:01:51 +0100

    Tests for the isset invoker trait

---
+++ tests/unit/traits/IssetInvokerTraitTest.php
@@ -0,0 +1,80 @@
+<?php
+
+use Aedart\Overload\Traits\IssetInvokerTrait;
+
+/**
+ * @coversDefaultClass Aedart\Overload\Traits\IssetInvokerTrait
+ */
+class IssetInvokerTraitTest extends \Codeception\TestCase\Test
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    /********************************************************************
+     * Helper methods
+     *******************************************************************/
+  
+    /**
+     * Returns a dummy class
+     * @return \IssetDummy
+     */
+    protected function getDummyClass(){
+	return new IssetDummy();
+    }
+        
+    /********************************************************************
+     * Actual tests
+     *******************************************************************/
+
+    /**
+     * @test
+     * @covers ::__isset
+     */
+    public function propertyIsset(){
+	$dummy = $this->getDummyClass();
+	$this->assertTrue(isset($dummy->name));
+    }
+
+    /**
+     * @test
+     * @covers ::__isset
+     */
+    public function propertyIsNotSet(){
+	$dummy = $this->getDummyClass();
+	$dummy->unsetName();
+	$this->assertFalse(isset($dummy->name));	
+    }
+
+    /**
+     * @test
+     * @covers ::__isset
+     */
+    public function issetOfNoneExistingProperty(){
+	$dummy = $this->getDummyClass();
+	$this->assertFalse(isset($dummy->age));	
+    }
+}
+
+/**
+ * Dummy class for given trait
+ */
+class IssetDummy {
+    
+    use IssetInvokerTrait;
+    
+    protected $name = 'Jill Anderson';
+    
+    public function unsetName(){
+	unset($this->name);
+    }
+}
