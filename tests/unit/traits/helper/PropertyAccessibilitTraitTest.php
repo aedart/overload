a9d0758741947fcd5e97d0582757a398e25261d1
Alin Eugen Deac <aedart@gmail.com>
2014-11-23 13:20:22 +0100

    Tests completed

--- tests/unit/traits/helper/PropertyAccessibilitTraitTest.php
+++ tests/unit/traits/helper/PropertyAccessibilitTraitTest.php
@@ -63,6 +63,7 @@ class PropertyAccessibilitTraitTest extends \Codeception\TestCase\Test
      * @covers ::getDefaultPropertyAccessibilityLevel
      * @covers ::getPropertyAccessibilityLevel
      * @covers ::setPropertyAccessibilityLevel
+     * @covers ::isPropertyAccessibilityLevelValid
      */
     public function getDefaultPropertyAccessibilityLevel(){
 	$method = $this->getMethod('getPropertyAccessibilityLevel');
@@ -70,12 +71,81 @@ class PropertyAccessibilitTraitTest extends \Codeception\TestCase\Test
 	$this->assertEquals(PropertyAccessibilityLevel::PROTECTED_LEVEL, $method->invoke($dummy));
     }
     
+    /**
+     * @test
+     * @covers ::getPropertyAccessibilityLevel
+     * @covers ::setPropertyAccessibilityLevel
+     * @covers ::isPropertyAccessibilityLevelValid
+     */
+    public function setAndGetPropertyAccessibilityLevel(){
+	$setter = $this->getMethod('setPropertyAccessibilityLevel');
+	$getter = $this->getMethod('getPropertyAccessibilityLevel');
+	$dummy = $this->getDummyClass();
+	
+	$setter->invoke($dummy, PropertyAccessibilityLevel::PRIVATE_LEVEL);
+	
+	$this->assertEquals(PropertyAccessibilityLevel::PRIVATE_LEVEL, $getter->invoke($dummy));
+    }
+
+    /**
+     * @test
+     * @covers ::setPropertyAccessibilityLevel
+     * @covers ::isPropertyAccessibilityLevelValid
+     * @expectedException \RangeException
+     */
+    public function setInvalidPropertyAccessibilityLevel(){
+	$setter = $this->getMethod('setPropertyAccessibilityLevel');
+	$dummy = $this->getDummyClass();
+	
+	$setter->invoke($dummy, -42);
+    }
+
+    /**
+     * @test
+     * @covers ::isPropertyAccessible
+     */
+    public function isPublicPropertyAccessible(){
+	$isAccessibleMethod = $this->getMethod('isPropertyAccessible');
+	$dummy = $this->getDummyClass();
+	$property = (new ReflectionClass($dummy))->getProperty('name');
+	
+	$this->assertTrue($isAccessibleMethod->invoke($dummy, $property));
+    }
     
+    /**
+     * @test
+     * @covers ::isPropertyAccessible
+     */
+    public function isProtectedPropertyAccessible(){
+	$isAccessibleMethod = $this->getMethod('isPropertyAccessible');
+	$dummy = $this->getDummyClass();
+	$property = (new ReflectionClass($dummy))->getProperty('age');
+	
+	$this->assertTrue($isAccessibleMethod->invoke($dummy, $property));
+    }
+    
+    /**
+     * @test
+     * @covers ::isPropertyAccessible
+     */
+    public function isPrivatePropertyAccessible(){
+	$isAccessibleMethod = $this->getMethod('isPropertyAccessible');
+	$dummy = $this->getDummyClass();
+	$property = (new ReflectionClass($dummy))->getProperty('height');
+	
+	// By default, protected is the highest level, so... this must be false!
+	$this->assertFalse($isAccessibleMethod->invoke($dummy, $property));
+    }
 }
 
 /**
  * Dummy class that uses the given trait
  */
 class PropertyAccessibilityTraitDummy {
+    
+    public $name = 'John Doe';
+    protected $age = 42;
+    private $height = 193;
+    
     use PropertyAccessibilityTrait;
 }
