23aeefceedd4c5490c8f5726dc82f52cae9a151b
Alin Eugen Deac <aedart@gmail.com>
2014-11-23 15:19:18 +0100

    Tests for unset invoker trait

---
+++ tests/unit/traits/UnsetInvokerTraitTest.php
@@ -0,0 +1,72 @@
+<?php
+
+use Aedart\Overload\Traits\UnsetInvokerTrait;
+
+/**
+ * @coversDefaultClass Aedart\Overload\Traits\UnsetInvokerTrait
+ */
+class UnsetInvokerTraitTest extends \Codeception\TestCase\Test
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    /********************************************************************
+     * Helper methods
+     *******************************************************************/
+  
+    /**
+     * Returns a dummy class
+     * @return \UnsetDummy
+     */
+    protected function getDummyClass(){
+	return new UnsetDummy();
+    }
+        
+    /********************************************************************
+     * Actual tests
+     *******************************************************************/
+
+    /**
+     * @test
+     * @covers ::__unset
+     */
+    public function unsetProperty(){
+	$dummy = $this->getDummyClass();
+	unset($dummy->name);
+	$this->assertFalse($dummy->isPropSet('name'));
+    }
+        
+    /**
+     * @test
+     * @covers ::__unset
+     * @expectedException Aedart\Overload\Exception\UndefinedPropertyException
+     */
+    public function attemptUnsetNoneExistingProperty(){
+	$dummy = $this->getDummyClass();
+	unset($dummy->age);	
+    }
+}
+
+/**
+ * Dummy class that uses given trait
+ */
+class UnsetDummy {
+    
+    use UnsetInvokerTrait;
+    
+    protected $name = 'Lilla Henrikson';
+    
+    public function isPropSet($propName){
+	return isset($this->$propName);
+    }
+}
