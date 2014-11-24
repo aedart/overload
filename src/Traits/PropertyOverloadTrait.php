fcbacd2b2627b2508acfdc4d667a52e73b83f265
Alin Eugen Deac <aedart@gmail.com>
2014-11-24 15:51:46 +0100

    A basic description added

--- src/Traits/PropertyOverloadTrait.php
+++ src/Traits/PropertyOverloadTrait.php
@@ -8,9 +8,17 @@ use Aedart\Overload\Traits\UnsetInvokerTrait;
 /**
  * Property Overload Trait
  * 
- * @todo Description of PropertyOverloadTrait
+ * <b>Description</b><br />
+ * Composite trait, which implements PHP's magic methods for "overloading"
+ * properties. Each property, however, have a defined getter and setter
+ * method implemented, or the give component will fail reads or writes
+ * attempts, on the given property
  * 
  * @see \Aedart\Overload\Interfaces\PropertyOverloadable
+ * @see \Aedart\Overload\Traits\GetterInvokerTrait
+ * @see \Aedart\Overload\Traits\SetterInvokerTrait
+ * @see \Aedart\Overload\Traits\IssetInvokerTrait
+ * @see \Aedart\Overload\Traits\UnsetInvokerTrait
  * @see \Aedart\Overload\Traits\Helper\PropertyAccessibilityTrait
  * 
  * @author Alin Eugen Deac <aedart@gmail.com>
